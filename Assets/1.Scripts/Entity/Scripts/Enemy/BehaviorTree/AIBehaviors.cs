e7994eb6304bfcb9256d64af693d0517bf17713f
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-06-20 21:58:55 +0900

    [Feat] BehaviorTree 기본 구조 작성
    * 각 노드별 스크립트 생성

---
+++ Assets/1.Scripts/Entity/Scripts/Enemy/BehaviorTree/AIBehaviors.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AIBehaviors
+{
+    public static INode.State MoveTo(Transform from, Vector3 to)
+    {
+        // 간단한 이동 로직이라던가..
+        return INode.State.FAILED;
+    }
+}
