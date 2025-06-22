013c47d47f92ce798c1f4122383c32eb8396b77f
yj6662 <yj6662@naver.com>
2025-06-22 17:20:48 +0900

    [feat] UIManager
    -feat UIManager
    -feat UIBase
    -feat UIPopup

---
+++ Assets/1.Scripts/UI/UIPopup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using _1.Scripts.Manager.Core;
+using _1.Scripts.Manager.Subs;
+
+namespace _1.Scripts.UI
+{
+    public abstract class UIPopup : MonoBehaviour
+    {
+        protected UIManager uiManager;
+
+        public virtual void Init(UIManager manager)
+        {
+            uiManager = manager;
+        }
+
+        public virtual void ClosePopup()
+        {
+            uiManager.ClosePopup(this);
+        }
+    }
+}
