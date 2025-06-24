e3be6b48dcefc4e62be9ec9ddeb3a97f12407801
Bangeunseong <bangeunseong@naver.com>
2025-06-24 10:01:02 +0900

    [feat] Updated Gun & Bullet & Player Condition Scripts

--- Assets/1.Scripts/Weapon/Scripts/Bullet.cs
+++ Assets/1.Scripts/Weapon/Scripts/Bullet.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using _1.Scripts.Interfaces;
+using _1.Scripts.Manager.Core;
 using UnityEngine;
 
 namespace _1.Scripts.Weapon.Scripts
@@ -8,12 +10,18 @@ namespace _1.Scripts.Weapon.Scripts
         [Header("Components")] 
         [SerializeField] private Rigidbody rigidBody;
 
-        [Header("Bullet Settings")] 
+        [Header("Bullet Presets")] 
         [SerializeField] private float appliedForce;
         [SerializeField] private float maxMoveDistance;
-        [SerializeField] private float currentMoveDistance;
+        [SerializeField] private Vector3 initializedPosition;
+        [SerializeField] private int damage;
         [SerializeField] private LayerMask hittableLayer;
         [SerializeField] private Vector3 direction;
+        
+        [Header("Bullet Settings")]
+        [SerializeField] private float drag;
+
+        private bool isAlreadyReached;
 
         private void Awake()
         {
@@ -22,26 +30,44 @@ namespace _1.Scripts.Weapon.Scripts
 
         private void OnEnable()
         {
+            isAlreadyReached = false;
             rigidBody.useGravity = false;
-            
+            rigidBody.drag = 0f;
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.angularVelocity = Vector3.zero;
         }
 
-        public void Initialize(float force, float maxDistance, Vector3 position, Vector3 dir, LayerMask layerToRemove)
+        private void FixedUpdate()
+        {
+            if (isAlreadyReached) return;
+            if (!((transform.position - initializedPosition).sqrMagnitude > maxMoveDistance * maxMoveDistance)) return;
+            rigidBody.useGravity = true;
+            rigidBody.drag = drag;
+            isAlreadyReached = true;
+        }
+
+        public void Initialize(Vector3 position, Vector3 dir, float maxDistance, float force, int dealtDamage, LayerMask hitLayer)
         {
             transform.position = position;
+            initializedPosition = position;
             transform.rotation = Quaternion.LookRotation(dir);
             
+            damage = dealtDamage;
             appliedForce = force; 
             maxMoveDistance = maxDistance;
             direction = dir;
-            hittableLayer &= ~layerToRemove;
-            
-            rigidBody.AddForce(direction * appliedForce);
+            hittableLayer = hitLayer;
+
+            rigidBody.velocity = direction * appliedForce;
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            
+            if (((1 << other.gameObject.layer) & hittableLayer) != 0)
+            {
+                if (other.TryGetComponent(out IDamagable damagable)){ damagable.OnTakeDamage(damage); }
+            }
+            CoreManager.Instance.objectPoolManager.Release(gameObject);
         }
     }
 }
