69572b46103c22c5f1b219891cd7cec44970e62d
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-06-25 13:16:52 +0900

    [feat] 레콘드론 1차 노드조립 (2-4)
    * 2-4까지 조립 완
    * 타이머 코루틴 컨트롤러에 추가
    * SuicideDroneAIController 미리 생성

--- Assets/1.Scripts/Entity/Scripts/NPC/AIControllers/Enemy/EnemyReconDroneAIController.cs
+++ Assets/1.Scripts/Entity/Scripts/NPC/AIControllers/Enemy/EnemyReconDroneAIController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using _1.Scripts.Entity.Scripts.NPC.AIBehaviors;
+using _1.Scripts.Entity.Scripts.NPC.AIBehaviors.Drone;
 using _1.Scripts.Entity.Scripts.NPC.AIBehaviors.Drone.ReconDrone;
 using _1.Scripts.Entity.Scripts.NPC.AIControllers;
 using _1.Scripts.Entity.Scripts.NPC.BehaviorTree;
@@ -16,8 +17,13 @@ namespace _1.Scripts.Entity.Scripts.NPC.AIControllers.Enemy
     /// </summary>
     public class EnemyReconDroneAIController : BaseNpcAI
     {
+        [Header("Alert")]
         private bool isAlerted = false;
 
+        [Header("Timer")] 
+        private Coroutine timerCoroutine = null;
+        private float timer = 0f;
+        
         protected override void Start()
         {
             statData = CoreManager.Instance.resourceManager.GetAsset<ReconDroneData>("ReconDroneData");
@@ -28,30 +34,76 @@ namespace _1.Scripts.Entity.Scripts.NPC.AIControllers.Enemy
         protected override void BuildTree()
         {
             // 공격 시퀀스 등록
+            // 1-1
             SequenceNode attackSequenceNode = new SequenceNode();
+            // 1-2
             ActionNode isPlayerInAttackRange = new ActionNode(new IsPlayerInAttackRange().Evaluate);
             ActionNode reconDroneAttack = new ActionNode(new ReconDroneAttacking().Evaluate);
             attackSequenceNode.Add(isPlayerInAttackRange);
             attackSequenceNode.Add(reconDroneAttack);
             
             // 추적 시퀀스 등록
+            // 2-1
             SequenceNode chaseSequenceNode = new SequenceNode();
+            // 2-2
             SelectorNode chaseCheckSelectorNode = new SelectorNode();
             SelectorNode chasingSelectorNode = new SelectorNode();
             chaseSequenceNode.Add(chaseCheckSelectorNode);
             chaseSequenceNode.Add(chasingSelectorNode);
-            
+            // 2-3
             SequenceNode checkAlertSequenceNode = new SequenceNode();
             SequenceNode checkPlayerInDetectRangeSequenceNode = new SequenceNode();
-            
-            
-            ActionNode isPlayerInDetectRange = new ActionNode(new IsPlayerInDetectRange().Evaluate);
-            rootNode.Add(isPlayerInDetectRange);
+            chaseCheckSelectorNode.Add(checkAlertSequenceNode);
+            chaseCheckSelectorNode.Add(checkPlayerInDetectRangeSequenceNode);
+            SequenceNode giveNewPathSequenceNode = new SequenceNode();
+            ActionNode returnRun = new ActionNode(new ReturnRun().Evaluate); 
+            chasingSelectorNode.Add(giveNewPathSequenceNode);
+            chaseCheckSelectorNode.Add(returnRun);
+            // 2-4
+            ActionNode isDroneAlerted = new ActionNode(new IsDroneAlerted().Evaluate);
+            ActionNode timerStart = new ActionNode(new AlertTimerStart().Evaluate);
+        }
+
+        public bool IsAlertedCheck()
+        {
+            return isAlerted;
         }
         
         public void SetAlert(bool alert)
         {
             isAlerted = alert;
+        }
+
+        public void TimerStartIfNull()
+        {
+            timerCoroutine ??= StartCoroutine(TimerStart()); // null이면 코루틴 실행
+        }
+
+        public void ResetAll()
+        {
+            SetAlert(false);
+            ResetTimer();
+            if (timerCoroutine != null)
+            {
+                StopCoroutine(timerCoroutine);
+                timerCoroutine = null;
+            }
+        }
+        
+        public void ResetTimer()
+        {
+            timer = 0f;
+        }
+        
+        public float TimerCheck()
+        {
+            return timer;
+        }
+
+        private IEnumerator TimerStart()
+        {
+            timer += Time.deltaTime;
+            yield return null;
         }
     }
 }
