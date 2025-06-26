20281516dcfdd35a0b9ed8a47ec44b86bd9f6036
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-06-27 05:31:57 +0900

    [feat] 프리팹 반영

--- Assets/1.Scripts/Entity/Scripts/NPC/AIBehaviors/IsPlayerInFOV.cs
+++ Assets/1.Scripts/Entity/Scripts/NPC/AIBehaviors/IsPlayerInFOV.cs
@@ -22,11 +22,13 @@ namespace _1.Scripts.Entity.Scripts.NPC.AIBehaviors
 
             // 시야 최대 거리 설정 (예: 100f)
             float maxViewDistance = 100f;
-
-            if (Physics.Raycast(origin, direction, out RaycastHit hit, maxViewDistance))
+            
+            Debug.DrawRay(origin + Vector3.up, direction * maxViewDistance, Color.red);
+            if (Physics.Raycast(origin + Vector3.up, direction, out RaycastHit hit, maxViewDistance))
             {
                 if (hit.collider.CompareTag("Player"))
                 {
+                    Debug.Log("플레이어 시야 내에 보임. 타이머 초기화");
                     return INode.State.SUCCESS;
                 }
             }
