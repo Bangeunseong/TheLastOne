c454e4a226047b508bca2e863d78ef8b63391a80
Bangeunseong <bangeunseong@naver.com>
2025-06-26 19:33:04 +0900

    [feat] Added Grenade Class and Interface that grenade will use

--- Assets/1.Scripts/Weapon/Scripts/Grenade/Grenade.cs
+++ Assets/1.Scripts/Weapon/Scripts/Grenade/Grenade.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using _1.Scripts.Weapon.Scripts.Common;
+using UnityEngine;
 
 namespace _1.Scripts.Weapon.Scripts.Grenade
 {
-    public class Grenade : MonoBehaviour
+    public class Grenade : BaseWeapon
     {
-        
+        public override void Initialize(GameObject ownerObj)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
