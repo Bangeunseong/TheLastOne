552680625927eca58b1938e6d9fba912d1ce0267
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-07-04 12:18:24 +0900

    [fix] Light 켜지지 않는 버그 수정
    * 원인 -> 루트노드가 Parallel Selector인데 IsNavMeshNotHasPath Conditional 노드에서 계속 라이트를 꺼서
    * 해결 -> 라이트를 끄는 작업은 따로 액션노드로 분리

--- Assets/1.Scripts/Entity/Scripts/NPC/AIBehaviors/BehaviorDesigner/Condition/IsNavMeshAgentHasPath.cs
+++ Assets/1.Scripts/Entity/Scripts/NPC/AIBehaviors/BehaviorDesigner/Condition/IsNavMeshAgentHasPath.cs
@@ -11,26 +11,10 @@ namespace _1.Scripts.Entity.Scripts.NPC.AIBehaviors.BehaviorDesigner.Condition
 	[TaskDescription("IsNavMeshAgentHasPath")]
 	public class IsNavMeshAgentHasPath : Conditional
 	{
-		public SharedAnimator animator;
-		public SharedBool shouldLookTarget;
 		public SharedNavMeshAgent agent;
-		public SharedLight light;
 		
 		public override TaskStatus OnUpdate()
 		{
-			AnimatorStateInfo stateInfo = animator.Value.GetCurrentAnimatorStateInfo(0);
-			if (!stateInfo.IsName("DroneBot_Hit1") &&
-			    !stateInfo.IsName("DroneBot_Hit2") &&
-			    !stateInfo.IsName("DroneBot_Hit3") &&
-			    !stateInfo.IsName("DroneBot_Hit4") &&
-			    !stateInfo.IsName("DroneBot_Idle1"))
-			{
-				animator.Value.SetTrigger(DroneAnimationHashData.Idle1);
-			}
-			light.Value.enabled = false;
-			
-			shouldLookTarget.Value = false; // 보통 노드구조 맨 끝자락에 배회할때 쓰니까 추가
-
 			if (agent.Value.remainingDistance < 0.1f || !agent.Value.hasPath)
 			{
 				return TaskStatus.Success;
