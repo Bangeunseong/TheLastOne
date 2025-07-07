4687c79a2240e13d6fc9dae39b4a0dcaedbba177
Bangeunseong <bangeunseong@naver.com>
2025-07-07 21:06:01 +0900

    [feat] Added Custom Shader Graph for glowing interactable weapons

--- Assets/1.Scripts/Weapon/Scripts/Common/DummyWeapon.cs
+++ Assets/1.Scripts/Weapon/Scripts/Common/DummyWeapon.cs
@@ -11,8 +11,20 @@ namespace _1.Scripts.Weapon.Scripts.Common
     {
         [field: Header("DummyGun Settings")]
         [field: SerializeField] public WeaponType Type { get; private set; }
-
+        [field: SerializeField] public SkinnedMeshRenderer[] Renderers { get; private set; }
+        
         public event Action OnPicked;
+
+        private void Awake()
+        {
+            if (Renderers.Length > 0) return;
+            Renderers = this.TryGetChildComponents<SkinnedMeshRenderer>("Gun");
+        }
+
+        private void Reset()
+        {
+            Renderers = this.TryGetChildComponents<SkinnedMeshRenderer>("Gun");
+        }
 
         public void OnInteract(GameObject ownerObj)
         {
