264b131d7d5f83bcac5ce353da6825b3364beb6b
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-07-09 21:15:06 +0900

    [refactor] 스탯컨트롤러 구조 변경

--- Assets/1.Scripts/Entity/Scripts/NPC/StatControllers/Base/BaseDroneStatController.cs
+++ Assets/1.Scripts/Entity/Scripts/NPC/StatControllers/Base/BaseDroneStatController.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using _1.Scripts.Entity.Scripts.NPC.AIBehaviors.BehaviorDesigner.SharedVariables;
+using _1.Scripts.Entity.Scripts.NPC.Data.AnimationHashData;
+using _1.Scripts.Entity.Scripts.NPC.Data.ForRuntime;
+using _1.Scripts.Entity.Scripts.Npc.StatControllers.Base;
+using _1.Scripts.Entity.Scripts.Player.Data;
+using _1.Scripts.Interfaces.Common;
+using _1.Scripts.Interfaces.NPC;
+using _1.Scripts.Manager.Core;
+using _1.Scripts.Static;
+using _1.Scripts.Util;
+using BehaviorDesigner.Runtime;
 using UnityEngine;
 
-public class BaseDroneStatController : MonoBehaviour
+namespace _1.Scripts.Entity.Scripts.NPC.StatControllers.Base
 {
-    // Start is called before the first frame update
-    void Start()
+    public abstract class BaseDroneStatController : BaseNpcStatController
     {
+        [SerializeField] protected int hackingFailAttackIncrease = 3;
+        [SerializeField] protected float hackingFailArmorIncrease = 3f;
+        [SerializeField] protected float hackingFailPenaltyDuration = 10f;
         
-    }
+        protected override void PlayDeathAnimation()
+        {
+            int[] deathHashes =
+            {
+                DroneAnimationHashData.Dead1,
+                DroneAnimationHashData.Dead2,
+                DroneAnimationHashData.Dead3
+            };
+            animator.SetTrigger(deathHashes[UnityEngine.Random.Range(0, deathHashes.Length)]);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        protected override void PlayHitAnimation()
+        {
+            int[] hitHashes =
+            {
+                DroneAnimationHashData.Hit1,
+                DroneAnimationHashData.Hit2,
+                DroneAnimationHashData.Hit3,
+                DroneAnimationHashData.Hit4
+            };
+            animator.SetTrigger(hitHashes[UnityEngine.Random.Range(0, hitHashes.Length)]);
+        }
+
+        protected override void HackingFailurePenalty()
+        {
+            int baseDamage = runtimeStatData.BaseDamage;
+            float baseArmor = runtimeStatData.Armor;
+            StartCoroutine(DamageAndArmorIncrease(baseDamage, baseArmor));
+            behaviorTree.SetVariableValue("shouldAlertNearBy", true);
+        }
         
+        private IEnumerator DamageAndArmorIncrease(int baseDamage, float baseArmor)
+        {
+            runtimeStatData.BaseDamage = baseDamage + hackingFailAttackIncrease;
+            runtimeStatData.Armor = baseArmor + hackingFailArmorIncrease;
+
+            yield return new WaitForSeconds(hackingFailPenaltyDuration);
+
+            runtimeStatData.BaseDamage = baseDamage;
+            runtimeStatData.Armor = baseArmor;
+        }
     }
 }
