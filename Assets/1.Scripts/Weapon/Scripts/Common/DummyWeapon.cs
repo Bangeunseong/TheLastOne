41dd3c47133ca0612e1e696d67d79fc2edba8510
Bangeunseong <bangeunseong@naver.com>
2025-07-22 12:55:56 +0900

    [feat] Added HackGun

--- Assets/1.Scripts/Weapon/Scripts/Common/DummyWeapon.cs
+++ Assets/1.Scripts/Weapon/Scripts/Common/DummyWeapon.cs
@@ -65,7 +65,7 @@ namespace _1.Scripts.Weapon.Scripts.Common
             {
                 if (player.PlayerCondition.Weapons[i] is Gun gun && gun.GunData.GunStat.Type == Type || 
                     player.PlayerCondition.Weapons[i] is GrenadeLauncher grenadeThrower && grenadeThrower.GrenadeData.GrenadeStat.Type == Type || 
-                    player.PlayerCondition.Weapons[i] is Crossbow crossbow && crossbow.HackData.HackStat.Type == Type)
+                    player.PlayerCondition.Weapons[i] is HackGun crossbow && crossbow.HackData.HackStat.Type == Type)
                 {
                     index = i; break;
                 }
@@ -79,7 +79,7 @@ namespace _1.Scripts.Weapon.Scripts.Common
             else
             {
                 var result = player.PlayerCondition.Weapons[index].OnRefillAmmo(
-                    player.PlayerCondition.Weapons[index] is Crossbow ? 4 :
+                    player.PlayerCondition.Weapons[index] is HackGun ? 5 :
                     player.PlayerCondition.Weapons[index] is GrenadeLauncher ? 6 : 
                         player.PlayerCondition.Weapons[index] is Gun gun && gun.GunData.GunStat.Type == WeaponType.Pistol ? 30 : 60);
                 if (!result) return;
