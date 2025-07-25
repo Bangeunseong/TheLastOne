ac47ce72784f0a981a16b9693e934a0db896df35
Bangeunseong <bangeunseong@naver.com>
2025-07-26 02:22:31 +0900

    [fix] Fixed Stage 2 Prefab

--- Assets/1.Scripts/Weapon/Scripts/Common/WeaponStat.cs
+++ Assets/1.Scripts/Weapon/Scripts/Common/WeaponStat.cs
@@ -6,6 +6,7 @@ namespace _1.Scripts.Weapon.Scripts.Common
     [Serializable] public class WeaponStat
     {
         [field: Header("Basic Weapon Settings")]
+        [field: SerializeField] public string Description { get; private set; }
         [field: SerializeField] public int MaxAmmoCount { get; private set; }
         [field: SerializeField] public float MaxWeaponRange { get; private set; }
         [field: SerializeField] public float Recoil { get; private set; }
