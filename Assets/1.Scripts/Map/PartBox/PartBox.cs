d1bd38eddf5856db15c09161d1f28b8e4af08ad7
Bangeunseong <bangeunseong@naver.com>
2025-07-29 14:38:05 +0900

    [chore] Added ItemBox Asset & Item Asset

--- Assets/1.Scripts/Map/PartBox/PartBox.cs
+++ Assets/1.Scripts/Map/PartBox/PartBox.cs
@@ -5,18 +5,27 @@ using _1.Scripts.Entity.Scripts.Player.Core;
 using _1.Scripts.Interfaces.Player;
 using _1.Scripts.Manager.Core;
 using _1.Scripts.Weapon.Scripts.Common;
+using AYellowpaper.SerializedCollections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace _1.Scripts.Map.PartBox
 {
+    public enum BoxType
+    {
+        Ammo,
+        Item,
+        Parts,
+    }
+    
     public class PartBox : MonoBehaviour, IInteractable
     {
-        [field: Header("References")]
-        [field: SerializeField] public int Id { get; private set; }
-        [field: SerializeField] public bool IsAlreadySearched { get; private set; }
+        [field: Header("Components")]
         [field: SerializeField] public Animator Animator { get; private set; }
         
+        [field: Header("References")]
+        [field: SerializeField] public int Id { get; private set; }
+        [field: SerializeField] public SerializedDictionary<BoxType, int> GenSettings { get; private set; } = new();
         
         private CoreManager coreManager;
         private Player player;
@@ -46,9 +55,23 @@ namespace _1.Scripts.Map.PartBox
             if (!ownerObj.TryGetComponent(out Player p) || isAlreadyOpened) return;
             
             isAlreadyOpened = true;
-            
-            if (p.PlayerWeapon.Weapons.Any(weapon => weapon.Value.TryCollectWeaponPart(partId)))
-                coreManager.SaveData_QueuedAsync();
+
+            foreach (var gen in GenSettings.Where(gen => gen.Value > 0))
+            {
+                switch (gen.Key)
+                {
+                    case BoxType.Ammo: 
+                        
+                        break;
+                    case BoxType.Item: 
+                        
+                        break;
+                    case BoxType.Parts:
+                        if (p.PlayerWeapon.Weapons.Any(weapon => weapon.Value.TryCollectWeaponPart(partId))) 
+                            coreManager.SaveData_QueuedAsync(); 
+                        break;
+                }
+            }
         }
 
         public void OnCancelInteract() { }
