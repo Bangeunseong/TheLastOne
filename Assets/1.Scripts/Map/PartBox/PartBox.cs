25391098ad7815a2252002dfc78de9fae7dce84e
Bangeunseong <bangeunseong@naver.com>
2025-08-01 12:54:48 +0900

    [feat] Added Navigation & 아이템 박스 오픈 시 저장, Save Point Trigger 시 저장

--- Assets/1.Scripts/Map/PartBox/PartBox.cs
+++ Assets/1.Scripts/Map/PartBox/PartBox.cs
@@ -5,6 +5,8 @@ using _1.Scripts.Entity.Scripts.Player.Core;
 using _1.Scripts.Interfaces.Player;
 using _1.Scripts.Item.Common;
 using _1.Scripts.Manager.Core;
+using _1.Scripts.Manager.Data;
+using _1.Scripts.Quests.Core;
 using _1.Scripts.Weapon.Scripts.Common;
 using AYellowpaper.SerializedCollections;
 using UnityEngine;
@@ -19,15 +21,21 @@ namespace _1.Scripts.Map.PartBox
         Parts,
     }
     
-    public class PartBox : MonoBehaviour, IInteractable
+    public class PartBox : MonoBehaviour, IInteractable, IGameEventListener
     {
+        public const int BaseItemBoxIndex = 1200;
+        
         [field: Header("Components")]
         [field: SerializeField] public Animator Animator { get; private set; }
         [field: SerializeField] public ParticleSystem ParticleSystem { get; private set; }
         [field: SerializeField] public List<Transform> SpawnPoints { get; private set; } = new();
         
         [field: Header("References")]
-        [field: SerializeField] public int Id { get; private set; }
+        [field: Tooltip("Target Id is for the Quest")]
+        [field: SerializeField] public int TargetId { get; private set; }
+        [field: Tooltip("Instance Id is for the check if is opened last time.")]
+        [field: SerializeField] public int InstanceId { get; private set; }
+        [field: Tooltip("Changing Value greater than 0 will spawn corresponding prop.")]
         [field: SerializeField] public SerializedDictionary<BoxType, int> GenSettings { get; private set; } = new();
         
         private CoreManager coreManager;
@@ -62,12 +70,14 @@ namespace _1.Scripts.Map.PartBox
         {
             coreManager = CoreManager.Instance;
             player = coreManager.gameManager.Player;
+            GameEventSystem.Instance.RegisterListener(this);
             
             var save = CoreManager.Instance.gameManager.SaveData;
             if (save is not { stageInfos: not null } ||
                 !save.stageInfos.TryGetValue(CoreManager.Instance.sceneLoadManager.CurrentScene, out var info)) return;
-            if (!info.completionDict.TryGetValue(Id, out var value) || value) return;
+            if (!info.completionDict.TryGetValue(BaseItemBoxIndex + InstanceId, out var value) || value) return;
             
+            GameEventSystem.Instance.UnregisterListener(this);
             isAlreadyOpened = true;
             gameObject.layer = LayerMask.NameToLayer("Default");
         }
@@ -77,17 +87,35 @@ namespace _1.Scripts.Map.PartBox
             if (!ownerObj.TryGetComponent(out Player p) || isAlreadyOpened) return;
             
             isAlreadyOpened = true;
-            var save = CoreManager.Instance.gameManager.SaveData;
-            if (save is { stageInfos: not null } && save.stageInfos.TryGetValue(CoreManager.Instance.sceneLoadManager.CurrentScene, out var info))
-                info.completionDict.TryAdd(Id, true);
             
             PlayOpeningAnimation();
+            
+            p.PlayerCondition.LastSavedPosition = p.transform.position;
+            p.PlayerCondition.LastSavedRotation = p.transform.rotation;
+            
+            GameEventSystem.Instance.RaiseEvent(TargetId);
+            GameEventSystem.Instance.RaiseEvent(BaseItemBoxIndex + InstanceId);
         }
 
         public void OnCancelInteract() { }
         
-        public void OnAnimationEnd()
+        public void OnEventRaised(int eventID)
         {
+            if (eventID != BaseItemBoxIndex + InstanceId) return;
+            
+            var save = coreManager.gameManager.SaveData;
+            if (save is { stageInfos: not null } && save.stageInfos.TryGetValue(coreManager.sceneLoadManager.CurrentScene, out var info))
+                info.completionDict.TryAdd(BaseItemBoxIndex + InstanceId, true);
+            
+            GameEventSystem.Instance.UnregisterListener(this);
+            coreManager.SaveData_QueuedAsync();
+        }
+        
+        private void PlayOpeningAnimation()
+        {
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            ParticleSystem.Play();
+            
             foreach (var gen in GenSettings.Where(gen => gen.Value > 0))
             {
                 switch (gen.Key)
@@ -98,12 +126,30 @@ namespace _1.Scripts.Map.PartBox
                         ammo.transform.SetPositionAndRotation(SpawnPoints.First().position, SpawnPoints.First().rotation);
                         if (ammo.TryGetComponent(out DummyWeapon ammoComp)) 
                             ammoComp.Initialize(false, coreManager.spawnManager.GetInstanceHashId(ammo, (int)type, ammo.transform)); 
+                        coreManager.spawnManager.DynamicSpawnedWeapons.TryAdd(ammoComp.InstanceId, new SerializableWeaponProp
+                        {
+                            type = type, 
+                            transform = new SerializableTransform
+                            {
+                                position = new SerializableVector3(ammo.transform.position), 
+                                rotation = new SerializableQuaternion(ammo.transform.rotation)
+                            }
+                        });
                         break;
                     case BoxType.Item:
                         var item = coreManager.objectPoolManager.Get(ItemType.Shield + "_Prefab");
                         item.transform.SetPositionAndRotation(SpawnPoints[1].position, SpawnPoints[1].rotation);
                         if (item.TryGetComponent(out DummyWeapon itemComp)) 
                             itemComp.Initialize(false, coreManager.spawnManager.GetInstanceHashId(item, (int)ItemType.Shield, item.transform)); 
+                        coreManager.spawnManager.DynamicSpawnedItems.TryAdd(itemComp.InstanceId, new SerializableItemProp
+                        {
+                            type = ItemType.Shield, 
+                            transform = new SerializableTransform
+                            {
+                                position = new SerializableVector3(item.transform.position), 
+                                rotation = new SerializableQuaternion(item.transform.rotation)
+                            }
+                        });
                         break;
                     case BoxType.Parts:
                         foreach (var weapon in player.PlayerWeapon.Weapons)
@@ -113,13 +159,7 @@ namespace _1.Scripts.Map.PartBox
                         throw new ArgumentOutOfRangeException();
                 }
             }
-            coreManager.SaveData_QueuedAsync(); 
-        }
-
-        private void PlayOpeningAnimation()
-        {
-            gameObject.layer = LayerMask.NameToLayer("Default");
-            ParticleSystem.Play();
+            
             Animator.SetTrigger(Open);
         }
         
