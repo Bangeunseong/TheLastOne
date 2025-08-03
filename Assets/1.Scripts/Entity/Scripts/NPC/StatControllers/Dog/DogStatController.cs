94333acb1159e83153fd9221e8308e932331a3d8
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-08-03 23:54:24 +0900

    [feat] Dog_ 해킹패널티 추가

--- Assets/1.Scripts/Entity/Scripts/NPC/StatControllers/Dog/DogStatController.cs
+++ Assets/1.Scripts/Entity/Scripts/NPC/StatControllers/Dog/DogStatController.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using _1.Scripts.Entity.Scripts.NPC.Data.AnimationHashData;
 using _1.Scripts.Entity.Scripts.NPC.Data.ForRuntime;
 using _1.Scripts.Entity.Scripts.NPC.Data.StatDataSO;
@@ -5,12 +6,21 @@ using _1.Scripts.Entity.Scripts.Npc.StatControllers.Base;
 using _1.Scripts.Entity.Scripts.NPC.StatControllers.Base;
 using _1.Scripts.Manager.Core;
 using _1.Scripts.Static;
+using _1.Scripts.Util;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace _1.Scripts.Entity.Scripts.NPC.StatControllers.Dog
 {
     public class DogStatController : BaseNpcStatController
     {
         private RuntimeDogStatData runtimeDogStatData;
+        
+        [Header("hackingFailPenalty")]
+        [SerializeField] protected int hackingFailAttackIncrease = 3;
+        [SerializeField] protected float hackingFailArmorIncrease = 3f;
+        [SerializeField] protected float hackingFailPenaltyDuration = 10f;
+        private CancellationTokenSource penaltyToken;
         
         protected override void Awake()
         {
@@ -19,7 +29,6 @@ namespace _1.Scripts.Entity.Scripts.NPC.StatControllers.Dog
             runtimeDogStatData = new RuntimeDogStatData(dogStatData);
             runtimeStatData = runtimeDogStatData;
         }
-        // 이후 runtimeSuicideDroneStatData 수정 시 베이스에 반영
         
         protected override void OnDisable()
         {
@@ -32,20 +41,41 @@ namespace _1.Scripts.Entity.Scripts.NPC.StatControllers.Dog
             base.OnEnable();
             animator.SetTrigger(DroneAnimationData.Idle1);
         }
-
+        
         protected override void PlayHitAnimation()
         {
             if (!IsStunned) behaviorTree.SetVariableValue(BehaviorNames.ShouldAlertNearBy, true);
         }
-
-        protected override void HackingFailurePenalty()
+        
+        protected override void HackingFailurePenalty() // 각 객체마다 패널티를 분리해야한다면 오버라이드를 자식으로 옮기기
         {
+            int baseDamage = runtimeStatData.BaseDamage;
+            float baseArmor = runtimeStatData.Armor;
             
+            penaltyToken?.Cancel();
+            penaltyToken?.Dispose();
+            penaltyToken = NpcUtil.CreateLinkedNpcToken();
+            
+            _= DamageAndArmorIncrease(baseDamage, baseArmor, penaltyToken.Token);
+            behaviorTree.SetVariableValue(BehaviorNames.ShouldAlertNearBy, true);
+        }
+        
+        private async UniTaskVoid DamageAndArmorIncrease(int baseDamage, float baseArmor, CancellationToken token)
+        {
+            runtimeStatData.BaseDamage = baseDamage + hackingFailAttackIncrease;
+            runtimeStatData.Armor = baseArmor + hackingFailArmorIncrease;
+
+            await UniTask.WaitForSeconds(hackingFailPenaltyDuration, cancellationToken:token);
+
+            runtimeStatData.BaseDamage = baseDamage;
+            runtimeStatData.Armor = baseArmor;
         }
 
         public override void DisposeAllUniTasks()
         {
             base.DisposeAllUniTasks();
+            penaltyToken?.Dispose();
+            penaltyToken = null;
         }
     }
 }
